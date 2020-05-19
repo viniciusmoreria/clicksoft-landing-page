2d1ec10539cbabfc03a060260e9bb32c8b98d45e
Vinícius Moreira <viniciusmoreeira@gmail.com>
2020-05-19 10:28:11 -0300

    fix: new sitemap and favicon model

---
+++ README.md
@@ -0,0 +1,1 @@
+Clicksoft Landing Page Project
--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,37 @@
-import App from 'next/app';
-import { NextSeo } from 'next-seo';
-import { ThemeProvider } from 'styled-components';
+import App from "next/app";
+import { NextSeo } from "next-seo";
+import { ThemeProvider } from "styled-components";
 
-import '../styles/style.css';
+import "../styles/style.css";
 
 const theme = {
   // Colors
-  primary: '#593792',
-  primaryDark: '#000',
-  primaryLight: '#fff',
-  background: '#fbfbfb',
-  accent: '#5BBFB4',
+  primary: "#593792",
+  primaryDark: "#000",
+  primaryLight: "#fff",
+  background: "#fbfbfb",
+  accent: "#5BBFB4",
 
   // Max Width
-  maxWidth: '980px',
+  maxWidth: "980px",
 
   // Flex
-  flex: 'display: flex; align-items: center;',
+  flex: "display: flex; align-items: center;",
   grid:
-    'display: grid;  align-items: center; justify-content: center; justify-items: center',
+    "display: grid;  align-items: center; justify-content: center; justify-items: center",
 
   // Form
   inputForm:
-    'font-size: 1.6rem; width: 100%; border-radius: 4px; border: 1px solid #dddddd; padding: 1rem; margin: 1rem 0',
+    "font-size: 1.6rem; width: 100%; border-radius: 4px; border: 1px solid #dddddd; padding: 1rem; margin: 1rem 0",
 
   //Breakpoints
 
   breakpoints: {
-    ss: '0px',
-    sm: '576px',
-    md: '768px',
-    lg: '992px',
-    xl: '1200px',
+    ss: "0px",
+    sm: "576px",
+    md: "768px",
+    lg: "992px",
+    xl: "1200px",
   },
 };
 
@@ -41,30 +41,30 @@ export default class MyApp extends App {
     return (
       <ThemeProvider theme={theme}>
         <NextSeo
-          title='Clicksoft - Aplicativos, sites, sistemas e marketing digital.'
-          description='Aplicativos, sites, sistemas e marketing digital.'
-          canonical='https://clicksoft-landing-page.now.sh/'
-          hrefLang='pt-BR'
+          title="Clicksoft - Aplicativos, sites, sistemas e marketing digital."
+          description="Aplicativos, sites, sistemas e marketing digital."
+          canonical="https://clicksoft-landing-page.now.sh/"
+          hrefLang="pt-BR"
           openGraph={{
-            url: 'https://clicksoft-landing-page.now.sh/logo.png',
+            url: "https://i.imgur.com/ktvItxJ.png",
             title:
-              'Clicksoft - Aplicativos, sites, sistemas e marketing digital.',
-            description: 'Aplicativos, sites, sistemas e marketing digital.',
+              "Clicksoft - Aplicativos, sites, sistemas e marketing digital.",
+            description: "Aplicativos, sites, sistemas e marketing digital.",
             images: [
               {
-                url: 'https://clicksoft-landing-page.now.sh/logo.png',
+                url: "https://i.imgur.com/ktvItxJ.png",
                 width: 800,
                 height: 600,
-                alt: 'Website Logo',
+                alt: "Website Logo",
               },
             ],
             site_name:
-              'Clicksoft - Aplicativos, sites, sistemas e marketing digital.',
+              "Clicksoft - Aplicativos, sites, sistemas e marketing digital.",
           }}
           twitter={{
-            handle: '@handle',
-            site: '@site',
-            cardType: 'summary_large_image',
+            handle: "@handle",
+            site: "@site",
+            cardType: "summary_large_image",
           }}
         />
         <Component {...pageProps} />
